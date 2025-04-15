40b8ec086d4b3561e902bc4bd54fc2a50a83499d
Vebnik <vebnik111@gmail.com>
2025-04-15 11:27:48 +0300

    Init

---
+++ unix/remove.sh
@@ -0,0 +1,9 @@
+current_dir=$PWD
+
+frontend_work_dir="$current_dir/work-dirs/ps-tms-frontend-work"
+importer_work_dir="$current_dir/work-dirs/ps-tms-importer-work"
+ps_tms_work_dir="$current_dir/work-dirs/ps-tms-work"
+
+rm -r $frontend_work_dir
+rm -r $importer_work_dir
+rm -r $ps_tms_work_dir
