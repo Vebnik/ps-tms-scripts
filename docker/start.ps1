ab91fec076fee05486389dfcc6ae13e6ffc9a7db
NikEvs <vebnik111@gmail.com>
2025-06-02 09:09:38 +0300

    Updated docker scripts

--- docker/start.ps1
+++ docker/start.ps1
@@ -1,2 +1,5 @@
+Write-Host "[INFO] Running ps-tms-ubuntu container ..."
+docker container start ps-tms-ubuntu
+
 Write-Host "[INFO] Running ps-tms-ubuntu ..."
 docker exec -it ps-tms-ubuntu /usr/bin/bash -c "cd ./release && /usr/sbin/ps-tms/front-end-server/start_servers.sh"
